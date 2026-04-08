d74435980a9810d4f6ece7241494d32f38e7ac62
Anil Panthi <apanthi21@gmail.com>
2026-04-08 20:17:34 +0545

    Prepeared for initial deployment

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,12 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(__filename)
 
+const NEXT_PUBLIC_SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL ||
+  (process.env.VERCEL_PROJECT_PRODUCTION_URL
+    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+    : 'http://localhost:3000')
+
 const nextConfig: NextConfig = {
   images: {
     localPatterns: [
@@ -13,7 +19,24 @@ const nextConfig: NextConfig = {
         pathname: '/api/media/file/**',
       },
     ],
+    remotePatterns: [
+      ...([NEXT_PUBLIC_SERVER_URL]
+        .filter(Boolean)
+        .map((item) => {
+          try {
+            const url = new URL(item as string)
+            return {
+              hostname: url.hostname,
+              protocol: url.protocol.replace(':', '') as 'http' | 'https',
+            }
+          } catch {
+            return null
+          }
+        })
+        .filter(Boolean) as any),
+    ],
   },
+  output: 'standalone',
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
@@ -23,6 +46,9 @@ const nextConfig: NextConfig = {
 
     return webpackConfig
   },
+  reactStrictMode: true,
+  serverExternalPackages: ['@payloadcms/db-postgres', 'drizzle-kit'],
+  transpilePackages: ['react-image-crop', '@payloadcms/next', '@payloadcms/ui'],
   turbopack: {
     root: path.resolve(dirname),
   },
