e259d36e1733aa78656cc0e2ee7e682fb6389e25
Anil Panthi <apanthi21@gmail.com>
2026-04-11 14:02:37 +0545

    Added not found page

---
+++ src/app/(frontend)/not-found.tsx
@@ -0,0 +1,36 @@
+'use client'
+import Link from 'next/link'
+import React, { useEffect } from 'react'
+import { useHeaderTheme } from '@/providers/HeaderTheme'
+
+import styles from './not-found.module.css'
+
+export default function NotFound() {
+  const { setHasHeroImage } = useHeaderTheme()
+
+  useEffect(() => {
+    setHasHeroImage(false)
+  }, [setHasHeroImage])
+
+  return (
+    <div className={styles.notFoundWrapper}>
+      <div className={styles.bgText}>404</div>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Oops! Page Not Found</h1>
+        <p className={styles.description}>
+          The page you&apos;re looking for doesn&apos;t exist or has been moved. 
+          Don&apos;t worry, even the best explorers get lost sometimes.
+        </p>
+
+        <div className={styles.actions}>
+          <Link href="/" className={styles.homeButton}>
+            Back to Home
+          </Link>
+          <Link href="/contact-us" className={styles.contactButton}>
+            Contact Support
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
