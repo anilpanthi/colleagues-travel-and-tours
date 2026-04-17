509c0e10890b3956137c3356118c6f55e05a05c6
Anil Panthi <apanthi21@gmail.com>
2026-04-17 16:14:25 +0545

    fixing brand image trimmed issue on safari

--- src/globals/Brand/Brand.client.tsx
+++ src/globals/Brand/Brand.client.tsx
@@ -5,6 +5,7 @@ import type { SiteSetting } from '@/payload-types'
 import Link from 'next/link'
 import { getMediaUrl } from '@/utilities/getMediaUrl'
 import styles from './Brand.module.scss'
+import Image from 'next/image'
 
 interface BrandClientProps {
   logos: SiteSetting['logos']
@@ -16,20 +17,15 @@ export const BrandClient: React.FC<BrandClientProps> = ({ logos, isScrolled }) =
   const logo = isScrolled ? logos?.logoLight : logos?.logoDark
   const logoObj = typeof logo === 'object' ? logo : null
   const src = logoObj?.url ? getMediaUrl(logoObj.url, logoObj.updatedAt) : null
-
+  // console.log(src)
   return (
-    <div className={styles.brandLink} style={{ marginTop: '1px' }}>
-      <Link href="/" className={styles.brandname}>
-        {src && (
-          <img
-            src={src}
-            alt={logoObj?.alt || 'Logo'}
-            width={logoObj?.width || undefined}
-            height={logoObj?.height || undefined}
-            className={styles.brandImage}
-            loading="eager"
-          />
-        )}
+    <div className={styles.brandLinkyyy} style={{ marginTop: '1px' }}>
+      {/* <Link href="/" className={styles.brandname}>
+        {src && <h2>Brandthenamesof test</h2>}
+      </Link> */}
+      <Link href={'/'}>
+        {/* <img src={src} alt="logo" height="57px" width="100px" /> */}
+        {src && <Image src={src} alt="logo" height={57} width={100} />}
       </Link>
     </div>
   )
