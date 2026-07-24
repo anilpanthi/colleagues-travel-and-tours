58dcd5c96e2a0ae243e0a834e4c3f01eeb610016
Anil Panthi <apanthi21@gmail.com>
2026-07-24 14:52:01 +0545

    making proper font sizes

--- src/blocks/Content/Component.tsx
+++ src/blocks/Content/Component.tsx
@@ -9,40 +9,37 @@ import Content from '@/components/ui/Content/Index'
 import style from './Component.module.scss'
 
 export const ContentBlock: React.FC<ContentBlockProps> = (props) => {
-	const { columns } = props
+  const { columns } = props
 
-	return (
-		<Content>
-			<div className={style.grid}>
-				{columns &&
-					columns.length > 0 &&
-					columns.map((col, index) => {
-						const { enableLink, link, richText, size } = col
+  return (
+    <Content>
+      <div className={style.grid}>
+        {columns &&
+          columns.length > 0 &&
+          columns.map((col, index) => {
+            const { enableLink, link, richText, size } = col
 
-						return (
-							<div
-								className={cn(style.column, style[size!])}
-								key={index}
-							>
-								{richText && (
-									<RichText
-										data={richText}
-										enableGutter={false}
-										enableProse={false}
-										className={style.content}
-										style={{
-											color: '#4a5568',
-											fontFamily: 'var(--font-jost)',
-											fontSize: '1.8rem',
-											lineHeight: '1.8',
-										}}
-									/>
-								)}
-								{enableLink && <CMSLink {...link} />}
-							</div>
-						)
-					})}
-			</div>
-		</Content>
-	)
+            return (
+              <div className={cn(style.column, style[size!])} key={index}>
+                {richText && (
+                  <RichText
+                    data={richText}
+                    enableGutter={false}
+                    enableProse={false}
+                    className={style.content}
+                    style={{
+                      color: '#4a5568',
+                      fontFamily: 'var(--font-jost)',
+                      fontSize: 'var(--type-body-lg)',
+                      lineHeight: 'var(--leading-reading)',
+                    }}
+                  />
+                )}
+                {enableLink && <CMSLink {...link} />}
+              </div>
+            )
+          })}
+      </div>
+    </Content>
+  )
 }
